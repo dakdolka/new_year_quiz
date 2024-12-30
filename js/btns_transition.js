b33cc346c302cb6c017168ba3ec137fd2d142a49
DolinovAK <dolinovak@mai.ru>
2024-12-31 00:18:50 +0300

    added base stuff

---
+++ js/btns_transition.js
@@ -0,0 +1,23 @@
+const backs = [
+    '../files/img/0.png',
+    '../files/img/1.png',
+    '../files/img/2.png',
+];
+
+const f_btns = document.querySelectorAll('.main-btn');
+const inf_btns = document.querySelectorAll('.ibp');
+console.log(inf_btns.length);
+const btns = [...f_btns, ...inf_btns];
+
+
+function setRandomBack() {
+    this.style.backgroundImage = `url(${backs[Math.floor(Math.random() * backs.length)]})`;
+}
+
+for (let btn of btns) {
+    btn.style.transition = 'all 0.3s ease-in-out'; // Переход только при изменении opacity
+    btn.addEventListener('mouseenter', setRandomBack);
+    btn.addEventListener('mouseleave', function () {
+        this.style.backgroundImage = ''; 
+    });
+}
---
+++ js/main.js
@@ -0,0 +1,56 @@
+if(JSON.parse(localStorage.getItem("ppls"))){
+  ppls=JSON.parse(localStorage.getItem("ppls"));
+}
+else{
+  ppls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+}
+
+function counter(elem, flag){
+    var ppl_id = parseInt(elem.id, 10);
+    if (flag){
+      ppls[ppl_id] = ppls[ppl_id] + 1;
+    }
+    else{
+      ppls[ppl_id] = -1;
+    }
+    localStorage.setItem("ppls", JSON.stringify(ppls));
+}
+
+// функция, которая будет запускаться при загрузке страницы
+function hideElements() {
+    var elements = document.getElementsByClassName('info');
+    var max_id = ppls[parseInt(document.body.id, 10)] % 3;
+    for (let i = 0; i < elements.length; i++) {
+        if (i > max_id) {
+          elements[i].style.display="none";
+        }
+      }
+  }
+
+function balls(){
+  console.log(ppls);
+    const btns = document.querySelectorAll('.main-btn');
+    for (let i = 0; i < btns.length; i++) {
+      if (ppls[i] >= 0){
+        btns[i].textContent = 300 - ((ppls[i] % 3) * 100);}
+      else{
+        btns[i].textContent = 'Победа!';
+        btns[i].style.color = 'green';
+      }
+
+    }
+  }
+  
+function clear_locals() {
+  ppls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  localStorage.setItem("ppls", JSON.stringify(ppls));
+}
+
+document.addEventListener('DOMContentLoaded', () =>{
+  if (!(window.location.pathname.split('/')[window.location.pathname.split('/').length - 1] == 'main.html')) {
+    hideElements();
+  }
+  else{
+    balls(true);
+  }
+});
