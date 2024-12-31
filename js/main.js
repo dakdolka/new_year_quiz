700952cdd68438353539b5e2e59a53ab103e6edf
_cptSkayt_ <lev.beregovoy.mai@gmail.com>
2024-12-31 19:38:07 +0300

    change colors

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 if(JSON.parse(localStorage.getItem("ppls"))){
   ppls=JSON.parse(localStorage.getItem("ppls"));
+  btn_colors=JSON.parse(localStorage.getItem("btn_colors"));
 }
 else{
   ppls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  btn_colors = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 }
 
 function super_game(value) {
@@ -10,14 +12,17 @@ function super_game(value) {
 }
 
 function counter(elem, flag){
-    var ppl_id = parseInt(elem.id, 10); 
-    if (flag){
-      ppls[ppl_id] = ppls[ppl_id] + 1;
-    }
-    else{
-      ppls[ppl_id] = -1;
-    }
-    localStorage.setItem("ppls", JSON.stringify(ppls));
+  let ppl_id = parseInt(elem.id, 10); 
+  if (flag){
+    ppls[ppl_id] = ppls[ppl_id] + 1;
+  }
+  else{
+    let group = prompt("Номер команды победителя");
+    btn_colors[ppl_id] = group;
+    ppls[ppl_id] = -1;
+  }
+  localStorage.setItem("ppls", JSON.stringify(ppls));
+  localStorage.setItem("btn_colors", JSON.stringify(btn_colors));
 }
 
 // функция, которая будет запускаться при загрузке страницы
@@ -33,20 +38,26 @@ function hideElements() {
 
 
 function balls(){
-  console.log(ppls);
-    const btns = document.querySelectorAll('.btns-text');
-    for (let i = 0; i < btns.length; i++) {
-      if (ppls[i] >= 0){
-        btns[i].textContent = 300 - ((ppls[i] % 3) * 100);}
-      else{
-        btns[i].textContent = 'Победа!';
-      }
-
+  const btns_text = document.querySelectorAll('.btns-text');
+  let colors = ['none', 'rgb(255, 100, 100)', 'rgb(100, 255, 100)', 'rgb(100, 100, 255)'];
+  for (let i = 0; i < btns_text.length; i++) {
+    if (ppls[i] >= 0){
+      btns_text[i].textContent = 300 - ((ppls[i] % 3) * 100);}
+    else{
+      btns_text[i].textContent = 'Закрыто';
     }
   }
+
+  const btns = document.getElementsByClassName('main-btn');
+  for (let i = 0; i < btns.length; i++) {
+    btns[i].style.backgroundColor = colors[btn_colors[i]];
+  }
+}
   
 function clear_locals() {
   ppls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  btn_colors = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  localStorage.setItem("btn_colors", JSON.stringify(btn_colors));
   localStorage.setItem("ppls", JSON.stringify(ppls));
   location.reload()
 }
@@ -56,6 +67,6 @@ document.addEventListener('DOMContentLoaded', () =>{
     hideElements();
   }
   else{
-    balls(true);
+    balls();
   }
 });
